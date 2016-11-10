c7dde4c2d0ab4e100c608295a196bb2bfb65d0c6
Terje Holene <terje.holene@avanade.com>
2016-11-10 12:34:48 +0100

    Initial solution++

---
+++ Altinn.ExternalUtilities.CorrBatchGenerator/Altinn.ExternalUtilities.CorrBatchGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Altinn.Batch.Correspondence;
+
+namespace Altinn.ExternalUtilities.CorrBatchGenerator
+{
+    /// <summary>
+    /// Console program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Console program start method.
+        /// </summary>
+        /// <param name="args">Console arguments.</param>
+        public static void Main(string[] args)
+        {
+            Correspondences cors = new Correspondences();
+        }
+    }
+}
---
+++ Altinn.ExternalUtilities.CorrBatchGenerator/Altinn.ExternalUtilities.CorrBatchGenerator/Schema/Readme.txt
@@ -0,0 +1,4 @@
+﻿Run commands in a Visual Studio Command line
+
+cd \project\Altinn.ExternalUtilities.CorrBatchGenerator\Altinn.ExternalUtilities.CorrBatchGenerator\Schema
+xsd schemas.altinn.no.services.intermediary.correspondence.2016.02.xsd /c /n:Altinn.Batch.Correspondence
