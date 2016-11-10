309cd4a6c91ce3f7aa8d02c5f18fd4044d766b77
anpreg <annamaria.preg@capgemini.com>
2016-11-10 13:19:43 +0100

    Sftp transfer with WinSCP

---
+++ Altinn.ExternalUtilities.CorrBatchGenerator/Altinn.ExternalUtilities.CorrBatchGenerator/Utils/SftpClientHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Configuration;
+
+using WinSCP;
+
+namespace Altinn.ExternalUtilities.CorrBatchGenerator.Utils
+{
+    public class SftpClientHelper
+    {
+        public static void TransferFiles()
+        {
+            try
+            {
+                // Setup session options
+                SessionOptions sessionOptions = new SessionOptions
+                {
+                    Protocol = Protocol.Sftp,
+                    HostName = ConfigurationManager.AppSettings["ftpHostName"],
+                    UserName = ConfigurationManager.AppSettings["ftpUserName"],
+                    Password = ConfigurationManager.AppSettings["ftpPassword"],
+                    SshHostKeyFingerprint = ConfigurationManager.AppSettings["sshHostKeyFingerprint"]
+                };
+
+                using (Session session = new Session())
+                {
+                    // Connect
+                    session.Open(sessionOptions);
+
+                    // Upload files
+                    TransferOptions transferOptions = new TransferOptions();
+                    transferOptions.TransferMode = TransferMode.Binary;
+
+                    TransferOperationResult transferResult;
+                    //transferResult = session.PutFiles(@"d:\toupload\*", "/home/user/", false, transferOptions);
+                    transferResult = session.PutFiles(ConfigurationManager.AppSettings["localPathToFilesToUpload"], ConfigurationManager.AppSettings["remotePath"], false, transferOptions);
+
+                    // Throw on any error
+                    transferResult.Check();
+
+                    // Print results
+                    foreach (TransferEventArgs transfer in transferResult.Transfers)
+                    {
+                        Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+        }
+    }
+}
